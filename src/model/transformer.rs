fb94f5b0b9d68d2d3d83150d1dc27f70bd0a5864
Minjie Zha <diego003@gmail.com>
2025-07-11 16:44:18 -0700

    implement RoPE

--- src/model/transformer.rs
+++ src/model/transformer.rs
@@ -24,6 +24,7 @@ impl Transformer {
         // a few convenience variables
         let config = &self.config;
         let dim = config.dim as usize;
+        let head_size = dim / config.n_heads as usize;
         let weights = &self.weights;
         let kv_dim = ((config.dim / config.n_heads) * config.n_kv_heads) as usize;
 
@@ -55,6 +56,28 @@ impl Transformer {
             nn::matmul(q, x, wq, dim, dim);
             nn::matmul(k, x, wk, kv_dim, dim);
             nn::matmul(v, x, wv, kv_dim, dim);
+
+            // RoPE relative positional encoding: complex-valued rotation q and k in each head
+            for i in (0..dim).step_by(2) {
+                // RoPE is applied within each head.
+                let index_within_head = i % head_size;
+                let freq = 1.0 / 10000.0f32.powf(index_within_head as f32 / head_size as f32);
+                let val = pos as f32 * freq;
+                let fcr = val.cos();
+                let fci = val.sin();
+                // when using grouped query attention, kv_dim is less than dim.
+                let rotn = if i < kv_dim { 2 } else { 1 };
+                for v in 0..rotn {
+                    let vec = if v == 0 {
+                        &mut q[i..i + 2]
+                    } else {
+                        &mut k[i..i + 2]
+                    };
+                    let (v0, v1) = (vec[i], vec[i + 1]);
+                    vec[i] = v0 * fcr - v1 * fci;
+                    vec[i + 1] = v0 * fci + v1 * fcr;
+                }
+            }
         }
         vec![]
     }
